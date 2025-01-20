613f02954174727bb1ef3199820466e2ae5fcbd8
Nestor M <ntor2004@gmail.com>
2025-01-20 08:29:34 +0100

    fix: correct user answer retrieval in checkAnswer function

--- quiz.js
+++ quiz.js
@@ -1,8 +1,8 @@
 function checkAnswer() {
     var correctAnswer = '4';
-    var userAnswer = document.querySelector('input[name="quiz"]:checked');
+    var userAnswer = document.querySelector('input[name="quiz"]:checked').value;
 
-    if ( userAnswer.value === correctAnswer) { 
+    if ( userAnswer === correctAnswer) { 
         document.getElementById('feedback').textContent = "Correct! Well done.";
     } else {
         document.getElementById('feedback').textContent = "That's incorrect. Try again!";
