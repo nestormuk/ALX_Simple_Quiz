cce8758afea88d1bc689f0cf839547530da49cd4
Nestor M <ntor2004@gmail.com>
2025-01-20 08:30:35 +0100

    style: format checkAnswer function for consistency

--- quiz.js
+++ quiz.js
@@ -2,7 +2,7 @@ function checkAnswer() {
     var correctAnswer = '4';
     var userAnswer = document.querySelector('input[name="quiz"]:checked').value;
 
-    if ( userAnswer === correctAnswer) { 
+    if (userAnswer === correctAnswer) { 
         document.getElementById('feedback').textContent = "Correct! Well done.";
     } else {
         document.getElementById('feedback').textContent = "That's incorrect. Try again!";
